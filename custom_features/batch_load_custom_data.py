fd640b2b48edd2f335a358cc0c980299fa2dd5b9
thill <thill@tntech.edu>
2024-06-14 22:31:35 -0500

    successfully tested batch_load_custom_data

---
+++ custom_features/batch_load_custom_data.py
@@ -0,0 +1,87 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# 
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+""" Batch mode in loading Scannet scenes with vertices and ground truth labels
+for semantic and instance segmentations
+
+Usage example: python ./batch_load_custom_data.py
+"""
+import os
+import sys
+import datetime
+import numpy as np
+from load_custom_data import export
+import pdb
+
+CUSTOM_DIR = 'CustomFeatures'
+TRAIN_SCAN_NAMES = [line.rstrip() for line in open(os.path.join(CUSTOM_DIR,'custom_train.txt'))]
+#LABEL_MAP_FILE = 'meta_data/custom-labels.combined.tsv'
+DONOTCARE_CLASS_IDS = np.array([])
+OBJ_CLASS_IDS = np.array([0,1,2])
+MAX_NUM_POINT = 50000
+OUTPUT_FOLDER = './custom_train_detection_data'
+
+def export_one_scan(scan_name, output_filename_prefix):    
+    #mesh_file = os.path.join(CUSTOM_DIR, scan_name, scan_name + '_vh_clean_2.ply')
+    part_type = scan_name.split('_')[1]
+    print('part_type:', part_type)
+
+    pcd_file = os.path.join(CUSTOM_DIR,'pcds', part_type, scan_name + '.pcd')
+    agg_file = os.path.join(CUSTOM_DIR,'labels', part_type, scan_name + '.aggregation.json')
+    seg_file = os.path.join(CUSTOM_DIR,'labels', part_type, scan_name + '.segs.json')
+    #meta_file = os.path.join(CUSTOM_DIR, scan_name + '.txt') # includes axisAlignment info for the train set scans.   
+    pcd_vertices, semantic_labels, instance_labels, instance_bboxes, instance2semantic = \
+        export(pcd_file, agg_file, seg_file, None)
+
+    print('returned from export function')        
+
+    mask = np.logical_not(np.in1d(semantic_labels, DONOTCARE_CLASS_IDS))
+    pcd_vertices = np.asarray(pcd_vertices)[mask,:]
+    semantic_labels = semantic_labels[mask]
+    instance_labels = instance_labels[mask]
+
+    num_instances = len(np.unique(instance_labels))
+    print('Num of instances: ', num_instances)
+
+    bbox_mask = np.in1d(instance_bboxes[:,-1], OBJ_CLASS_IDS)
+    instance_bboxes = instance_bboxes[bbox_mask,:]
+    print('Num of care instances: ', instance_bboxes.shape[0])
+
+    N = pcd_vertices.shape[0]
+    if N > MAX_NUM_POINT:
+        choices = np.random.choice(N, MAX_NUM_POINT, replace=False)
+        pcd_vertices = pcd_vertices[choices, :]
+        semantic_labels = semantic_labels[choices]
+        instance_labels = instance_labels[choices]
+
+    np.save(output_filename_prefix+'_vert.npy', pcd_vertices)
+    np.save(output_filename_prefix+'_sem_label.npy', semantic_labels)
+    np.save(output_filename_prefix+'_ins_label.npy', instance_labels)
+    np.save(output_filename_prefix+'_bbox.npy', instance_bboxes)
+    print('output vertices, semantic labels, instance labels, and instance boxes saved as .npy files')    
+
+def batch_export():
+    if not os.path.exists(OUTPUT_FOLDER):
+        print('Creating new data folder: {}'.format(OUTPUT_FOLDER))                
+        os.mkdir(OUTPUT_FOLDER)        
+        
+    for scan_name in TRAIN_SCAN_NAMES:
+        print('-'*20+'begin')
+        print(datetime.datetime.now())
+        print(scan_name)
+        output_filename_prefix = os.path.join(OUTPUT_FOLDER, scan_name) 
+        if os.path.isfile(output_filename_prefix+'_vert.npy'):
+            print('File already exists. skipping.')
+            print('-'*20+'done')
+            continue
+        try:            
+            export_one_scan(scan_name, output_filename_prefix)
+        except Exception as e: 
+            print(e)
+            print('Failed export scan: %s'%(scan_name))            
+        print('-'*20+'done')
+
+if __name__=='__main__':    
+    batch_export()
---
+++ custom_features/load_custom_data.py
@@ -0,0 +1,168 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# 
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+""" Load CustomFeatures scenes with vertices and ground truth labels
+for semantic and instance segmentations
+"""
+
+# python imports
+import math
+import os, sys, argparse
+import inspect
+import json
+import pdb
+
+try:
+    import numpy as np
+except:
+    print("Failed to import numpy package.")
+    sys.exit(-1)
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+import custom_utils
+
+# function to get instance segmentation information from json
+def read_aggregation(filename):
+    assert os.path.isfile(filename)
+    object_id_to_segs = {}
+    label_to_segs = {}
+    with open(filename) as f:
+        data = json.load(f)
+        num_objects = len(data['segGroups'])    
+        for i in range(num_objects):
+            object_id = data['segGroups'][i]['objectId'] # instance ids are 1-indexed
+            label = data['segGroups'][i]['label']
+            segs = data['segGroups'][i]['segments']
+            object_id_to_segs[object_id] = segs
+            if label in label_to_segs:
+                label_to_segs[label].extend(segs)
+            else:
+                label_to_segs[label] = segs
+    return object_id_to_segs, label_to_segs
+
+# function to get semantic segmentation information from json
+def read_segmentation(filename):
+    assert os.path.isfile(filename)
+    seg_to_verts = {}
+    with open(filename) as f:
+        data = json.load(f)
+        num_verts = len(data['segIndices'])
+        for i in range(num_verts):
+            seg_id = data['segIndices'][i]
+            if seg_id in seg_to_verts:
+                seg_to_verts[seg_id].append(i)
+            else:
+                seg_to_verts[seg_id] = [i]
+    return seg_to_verts, num_verts
+
+
+def export(pcd_file, agg_file, seg_file, output_file=None):
+    """ points are XYZ RGB (RGB in 0-255),
+    semantic label as nyu40 ids,
+    instance label as 1-#instance,
+    box as (cx,cy,cz,dx,dy,dz,semantic_label)
+    """
+    #label_map = scannet_utils.read_label_mapping(label_map_file,
+    #    label_from='raw_category', label_to='nyu40id')    
+    label_map = {'none':0, 'plate':1, 'inside_fillet':2, 'inside_corner':3}
+    
+    import open3d as o3d
+    pcd = o3d.io.read_point_cloud(pcd_file)
+    pcd_vertices=pcd.points
+    #mesh_vertices = scannet_utils.read_mesh_vertices_rgb(mesh_file)
+    print('%d vertices loaded from pcd file'%len(pcd_vertices))
+
+    ## Load scene axis alignment matrix # skip axis alignment for now
+    #lines = open(meta_file).readlines()
+    #for line in lines:
+    #    if 'axisAlignment' in line:
+    #        axis_align_matrix = [float(x) \
+    #            for x in line.rstrip().strip('axisAlignment = ').split(' ')]
+    #        break
+   
+    #axis_align_matrix = np.array(axis_align_matrix).reshape((4,4))
+    #pts = np.ones((pcd_vertices.shape[0], 4))
+    #pts[:,0:3] = pcd_vertices[:,0:3]
+    #pts = np.dot(pts, axis_align_matrix.transpose()) # Nx4
+    #pcd_vertices[:,0:3] = pts[:,0:3]
+
+    print('reading aggregation file: %s'%agg_file)
+    # Load semantic and instance labels
+    object_id_to_segs, label_to_segs = read_aggregation(agg_file)
+    print('reading aggregation file complete')
+    print('label_to_segs: ', label_to_segs)
+
+    print('reading segmentation file: %s'%seg_file)
+    seg_to_verts, num_verts = read_segmentation(seg_file)
+    print('reading segmentation file complete')
+
+
+    label_ids = np.zeros(shape=(num_verts), dtype=np.uint32) # 0: unannotated
+    object_id_to_label_id = {}
+    for label, segs in label_to_segs.items():    
+        label_id = label_map[label]
+        for seg in segs:
+            verts = seg_to_verts[seg]
+            label_ids[verts] = label_id
+    print('populated label_ids')
+
+    instance_ids = np.zeros(shape=(num_verts), dtype=np.uint32) # 0: unannotated
+    num_instances = len(np.unique(list(object_id_to_segs.keys())))
+    for object_id, segs in object_id_to_segs.items():
+        for seg in segs:
+            verts = seg_to_verts[seg]
+            instance_ids[verts] = object_id
+            if object_id not in object_id_to_label_id:
+                object_id_to_label_id[object_id] = label_ids[verts][0]
+    print('populated instance_ids')            
+
+    instance_bboxes = np.zeros((num_instances,7))
+    for obj_id in object_id_to_segs:
+        label_id = object_id_to_label_id[obj_id]
+        obj_pc = np.asarray(pcd_vertices)[instance_ids==obj_id, 0:3]
+        if len(obj_pc) == 0: continue
+        # Compute axis aligned box
+        # An axis aligned bounding box is parameterized by
+        # (cx,cy,cz) and (dx,dy,dz) and label id
+        # where (cx,cy,cz) is the center point of the box,
+        # dx is the x-axis length of the box.
+        xmin = np.min(obj_pc[:,0])
+        ymin = np.min(obj_pc[:,1])
+        zmin = np.min(obj_pc[:,2])
+        xmax = np.max(obj_pc[:,0])
+        ymax = np.max(obj_pc[:,1])
+        zmax = np.max(obj_pc[:,2])
+        bbox = np.array([(xmin+xmax)/2, (ymin+ymax)/2, (zmin+zmax)/2,
+            xmax-xmin, ymax-ymin, zmax-zmin, label_id])
+        # NOTE: this assumes obj_id is in 1,2,3,.,,,.NUM_INSTANCES
+        instance_bboxes[obj_id-1,:] = bbox 
+    print('generated instance_bboxes')    
+            
+
+    if output_file is not None:
+        np.save(output_file+'_vert.npy', pcd_vertices)
+        np.save(output_file+'_sem_label.npy', label_ids)
+        np.save(output_file+'_ins_label.npy', instance_ids)
+        np.save(output_file+'_bbox.npy', instance_bboxes)
+
+    return pcd_vertices, label_ids, instance_ids,\
+        instance_bboxes, object_id_to_label_id
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--scan_path', required=True, help='path to scannet scene (e.g., data/ScanNet/v2/scene0000_00')
+    parser.add_argument('--output_file', required=True, help='output file')
+    parser.add_argument('--label_map_file', required=True, help='path to scannetv2-labels.combined.tsv')
+    opt = parser.parse_args()
+
+    scan_name = os.path.split(opt.scan_path)[-1]
+    pcd_file = os.path.join(opt.scan_path, scan_name + '.pcd')
+    agg_file = os.path.join(opt.scan_path, scan_name + '.aggregation.json')
+    seg_file = os.path.join(opt.scan_path, scan_name + '.segs.json')
+    #meta_file = os.path.join(opt.scan_path, scan_name + '.txt') # includes axisAlignment info for the train set scans.
+    export(pcd_file, agg_file, seg_file, opt.label_map_file, opt.output_file)
+
+if __name__ == '__main__':
+    main()
